15ac2782b97da19e7a5c65ec8dabdc9ffc6f3e76
CreativeJuiz <CreativeJuiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-23 08:59:55 +0000

    Styles bug fixed
    git-svn-id: https://plugins.svn.wordpress.org/juiz-social-post-sharer/trunk@628898 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- juiz-social-post-sharer.php
+++ juiz-social-post-sharer.php
@@ -4,7 +4,7 @@ Plugin Name: Juiz Social Post Sharer
 Plugin URI: http://www.creativejuiz.fr/blog/
 Description: Add buttons after your posts to allow visitors share your content (includes no JavaScript mode). The setting page is located in *Settings* submenu. <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=P39NJPCWVXGDY&amp;lc=FR&amp;item_name=Juiz%20Social%20Post%20Sharer%20%2d%20WP%20Plugin&amp;item_number=%23wp%2djsps&amp;currency_code=EUR&amp;bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted">Donate</a>
 Author: Geoffrey Crofte
-Version: 1.1.0
+Version: 1.1.1
 Author URI: http://crofte.fr
 License: GPLv2 or later 
 */
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 define( 'JUIZ_SPS_PLUGIN_NAME',	 'Juiz Social Post Sharer' );
-define( 'JUIZ_SPS_VERSION',		 '1.1.0' );
+define( 'JUIZ_SPS_VERSION',		 '1.1.1' );
 define( 'JUIZ_SPS_FILE',		 __FILE__ );
 define( 'JUIZ_SPS_DIRNAME',		 basename( dirname( __FILE__ ) ) );
 define( 'JUIZ_SPS_PLUGIN_URL',	 plugin_dir_url( __FILE__ ));
@@ -63,9 +63,9 @@ if (!is_admin()) {
 			$juiz_sps_options = get_option( JUIZ_SPS_SETTING_NAME );
 			if(is_array($juiz_sps_options)) {
 				if( is_numeric( $juiz_sps_options['juiz_sps_style'] ) )
-					wp_enqueue_style( 'juiz_sps_styles', JUIZ_SPS_PLUGIN_URL.'/css/'.JUIZ_SPS_SLUG.'-'.$juiz_sps_options['juiz_sps_style'].'.min.css', false, false, 'all' );
+					wp_enqueue_style( 'juiz_sps_styles', JUIZ_SPS_PLUGIN_URL.'css/'.JUIZ_SPS_SLUG.'-'.$juiz_sps_options['juiz_sps_style'].'.min.css', false, false, 'all' );
 				if(is_numeric( $juiz_sps_options['juiz_sps_counter'] ) && $juiz_sps_options['juiz_sps_counter'] == 1 )
-					wp_enqueue_script( 'juiz_sps_scripts', JUIZ_SPS_PLUGIN_URL.'/js/'.JUIZ_SPS_SLUG.'.js', array('jquery'), false, true );
+					wp_enqueue_script( 'juiz_sps_scripts', JUIZ_SPS_PLUGIN_URL.'js/'.JUIZ_SPS_SLUG.'.js', array('jquery'), false, true );
 			}
 		}
 	}
@@ -97,8 +97,8 @@ if (!is_admin()) {
 					$after_last_i = apply_filters('juiz_sps_after_last_item', '');
 
 					// classes and attributes options
-					$juiz_sps_target_link = ($juiz_sps_options['juiz_sps_target_link']==1) ? ' target="_blank"' : '';
-					$juiz_sps_hidden_name_class = ($juiz_sps_options['juiz_sps_hide_social_name']==1) ? ' juiz_sps_hide_name' : '';
+					$juiz_sps_target_link = (isset($juiz_sps_options['juiz_sps_target_link']) && $juiz_sps_options['juiz_sps_target_link']==1) ? ' target="_blank"' : '';
+					$juiz_sps_hidden_name_class = (isset($juiz_sps_options['juiz_sps_hide_social_name']) && $juiz_sps_options['juiz_sps_hide_social_name']==1) ? ' juiz_sps_hide_name' : '';
 
 					// beginning markup
 					$juiz_sps_content  = $before_the_sps_content;
